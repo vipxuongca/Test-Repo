d0066733588e399a2ceee6c1d6cd79af39a47dec
vipxuongca <100637225+vipxuongca@users.noreply.github.com>
2024-02-27 23:25:36 +0700

    This is the first ever commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 integers: ");
+            Console.Write("a = ");
+            int.TryParse(Console.ReadLine(), out int a);
+            Console.Write("b = ");
+            int.TryParse(Console.ReadLine(), out int b);
+            Console.WriteLine($"The sum of {a} and {b} is {Sum(a, b)}");
+            Console.WriteLine("Enter any key to close...");
+            Console.ReadLine();
+        }
+
+        static int Sum(int a, int b)
+        {
+            int sum = a+b;
+            return sum;
+        }
+    }
+}
